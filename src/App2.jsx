0b3fc508fbaef77fc1aa78d19af656ba870bcab1
RNanWP <renan92011@hotmail.com>
2025-02-14 17:41:12 -0300

    Adicionando Comentários

--- src/App2.jsx
+++ src/App2.jsx
@@ -1,17 +1,38 @@
-
-
 // Nomeção de Componente
-    // PascalCase
-   //  Nomeação de variavel, Função...
-   // camelCase
-
+//    PascalCase
+//    Nomeação de variavel, Função...
+//    camelCase
 
 // export default, posso importar com qualquer nome e sem chaves
 // export "sem default", só posso importar entre chaves e com nome
 
 // Self closing tag
-   // <Header><Header>
-   // <Header />
+//    <Header><Header>
+//    <Header />
 
 // Nomeação de classe em CSS
+//    Metodologia BEM
+//    Blocka
+//    Elements
+//    Modifiers
+//    bloco__elemento--modificador
+//    header__link
+//    header--link--small
+
+// tag vazia em React se chama Fragment ou Fragmento
+
+// Componente recebem "props"
+// CTRL + ; = Comentário em qualquer linguagem
+// ALT + 124 = Barra Vertical ||
+
+// Usando função em JS "ex: useLocation()", sempre usilizando o nome da função e logo os parenteses
+
+
 // 
+// Preciso pesquisar mais sobre desestruturação em JS
+// 
+
+// Nomeação de classes
+//    Kebabe-case 
+
+// API signitica Application Programing Interface
