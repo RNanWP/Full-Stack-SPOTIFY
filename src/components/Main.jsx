0e5fbbe5dd6f89dcd9a159a3fcfe975c7c125f25
RNanWP <renan92011@hotmail.com>
2025-02-19 17:47:22 -0300

    Escrevendo as Classes e o CSS do Single Item

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,4 +1,6 @@
 import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCirclePlay } from "@fortawesome/free-solid-svg-icons";
 
 const Main = () => {
   return (
@@ -12,13 +14,20 @@ const Main = () => {
         </div>
 
         <div className="single-item">
-          <img
-            className="single-item__image"
-            src="https://i.scdn.co/image/ab67616d00001e022774b00531d558bc19e12a24"
-            alt="Imagem do Artista X"
-          />
+          <div className="single-item__div-image-button">
+            <div className="single-item__div-image">
+              <img
+                className="single-item__image"
+                src="https://i.scdn.co/image/ab67616d00001e022774b00531d558bc19e12a24"
+                alt="Imagem do Artista X"
+              />
+            </div>
 
-          <FontAwesomeIcon icon="fa-solid fa-circle-play" />
+            <FontAwesomeIcon
+              className="single-item__icon"
+              icon={faCirclePlay}
+            />
+          </div>
 
           <p>Henrique & Juliano</p>
           <p>Artista</p>
