f1abd61c70e3af37cffbd5389b5cea58b35a1a35
RNanWP <renan92011@hotmail.com>
2025-02-21 21:05:00 -0300

    Criando paginas

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
 import Header from "./components/Header";
 import Main from "./components/Main";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    <>
+    <BrowserRouter>
       <Header />
-      <Main/>
-    </>
+      <Main />
+    </BrowserRouter>
   );
-};
+}
 
 export default App;
