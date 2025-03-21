7125905e1b9638d2d006b779410182724724fbbd
RNanWP <renan92011@hotmail.com>
2025-03-21 07:57:34 -0300

    Adicionando o caminho Artist no back

--- back-end/api/server.js
+++ back-end/api/server.js
@@ -1,7 +1,28 @@
 //  API significa Application Programing Interface
+// POST, GET, PUT, DELETE
+// CRUD - Create, Read, Update e Delete
+// Endpoint
+// "Cd pasta do back
+//     - node --watch ./"
+
 import express from "express";
+import { artistArray } from "../../front-end/src/assets/database/artists.js";
 
 const app = express();
 const PORT = 3003;
 
-app.listen(PORT, () => console.log(`Servidor está escutando na porta ${PORT}`));
+app.get("/", (request, response) => {
+  response.send("Olá Mundo! Agora não preciso fica atualizando!");
+});
+
+app.get("/artists", (request, response) => {
+  response.send("artistArray");
+});
+
+app.get("/songs", (request, response) => {
+  response.send("Olá Mundo! Agora não preciso fica atualizando!");
+});
+
+app.listen(PORT, () => {
+  console.log(`Servidor está escutando na porta ${PORT}`);
+});
