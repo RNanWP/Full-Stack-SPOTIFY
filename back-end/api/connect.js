7aa1677f1e58a1ec4f18e1d4012038bf7162064e
RNanWP <renan92011@hotmail.com>
2025-03-24 07:49:03 -0300

    Tentando conectar api e banco de dados ao site

--- back-end/api/connect.js
+++ back-end/api/connect.js
@@ -1,1 +1,15 @@
-const URI = "mongodb+srv://<RNanWP>:<Renantimao@123>@cluster0.2qagk.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+// JavaScript Assincrono
+// Await & Async
+// FullFilled
+
+import { MongoClient } from "mongodb";
+
+const URI =
+  "mongodb+srv://Projeto 0:Renantimao123@cluster0.2qagk.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+
+const client = new MongoClient(URI);
+
+const db = client.db("Spotify");
+const songCollection = await db.collection("songs").find({}).toArray();
+
+console.log(songCollection);
