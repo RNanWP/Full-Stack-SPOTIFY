4a81d1f146a87b3baade786d1b95490e4937f008
RNanWP <renan92011@hotmail.com>
2025-03-25 20:46:27 -0300

    Atualizando Artist e Songs

--- front-end/api/api.js
+++ front-end/api/api.js
@@ -6,6 +6,6 @@ const responseArtists = await axios.get(`${URL}/artists`);
 const responseSongs = await axios.get(`${URL}/songs`);
 
 export const artistArray = responseArtists.data;
-export const songstArray = responseSongs.data;
+export const songsArray = responseSongs.data;
 
 // console.log(responseArtists);
