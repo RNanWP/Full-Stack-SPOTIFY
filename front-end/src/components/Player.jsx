13bec90a144ca79d260ee8ebe41b55e4774f1d09
RNanWP <renan92011@hotmail.com>
2025-04-05 19:50:55 -0300

    Criando o estado de play e pause

--- front-end/src/components/Player.jsx
+++ front-end/src/components/Player.jsx
@@ -1,11 +1,13 @@
-import React from "react";
+import React, { useState } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
   faBackwardStep,
   faCirclePlay,
+  faCirclePause,
   faForwardStep,
 } from "@fortawesome/free-solid-svg-icons";
 import { Link } from "react-router-dom";
+import { useRef } from "react";
 
 const Player = ({
   duration,
@@ -13,6 +15,14 @@ const Player = ({
   randomId2FromArtist,
   audio,
 }) => {
+  const audioPlayer = useRef();
+  const [isPlaying, setIsPlaying] = useState(false);
+
+  const playPause = () => {
+    isPlaying ? audioPlayer.current.pause() : audioPlayer.current.play();
+    setIsPlaying(!isPlaying);
+  };
+
   return (
     <div className="player">
       <div className="player__controllers">
@@ -22,7 +32,8 @@ const Player = ({
 
         <FontAwesomeIcon
           className="player__icon player__icon--play"
-          icon={faCirclePlay}
+          icon={isPlaying ? faCirclePause : faCirclePlay}
+          onClick={() => playPause()}
         />
         <Link to={`/song/${randomId2FromArtist}`}>
           <FontAwesomeIcon className="player__icon" icon={faForwardStep} />
@@ -35,10 +46,10 @@ const Player = ({
         <div className="player__bar">
           <div className="player__bar-progress"></div>
         </div>
-          <p className="player__time">{duration}</p>
-        </div>
-        <audio src={audio}></audio>
+        <p className="player__time">{duration}</p>
       </div>
+      <audio ref={audioPlayer} src={audio}></audio>
+    </div>
   );
 };
 
