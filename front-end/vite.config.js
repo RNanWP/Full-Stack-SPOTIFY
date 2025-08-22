187b9c45261c86683032d540e4574d726a6704fd
RNanWP <renan92011@hotmail.com>
2025-08-21 21:29:28 -0300

    feat: atualizando vite

--- front-end/vite.config.js
+++ front-end/vite.config.js
@@ -7,9 +7,20 @@ export default defineConfig({
   esbuild: {
     target: "esnext",
   },
-  server: {
-    headers: {
-      "Content-Type": "application/javascript",
-    },
-  },
 });
+
+// import { defineConfig } from "vite";
+// import react from "@vitejs/plugin-react";
+
+// // https://vite.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+//   esbuild: {
+//     target: "esnext",
+//   },
+//   server: {
+//     headers: {
+//       "Content-Type": "application/javascript",
+//     },
+//   },
+// });
